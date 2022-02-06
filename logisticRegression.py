4d23fe5ccdbd3684cd927217c2f446f8414d4769
Josip-Rizner <62592307+Josip-Rizner@users.noreply.github.com>
2022-02-06 19:33:35 +0100

    Delete logisticRegression.py

--- logisticRegression.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-X_train = pd.read_csv("MachineLearningX_train.csv").values
-X_test = pd.read_csv("ready data\\X_test.csv").values
-y_train = pd.read_csv("ready data\\y_train.csv").values.reshape(-1,)
-y_test = pd.read_csv("\\ready data\\y_test.csv").values.reshape(-1,)
